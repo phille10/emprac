dad11e1271433865a305c8bb169fc2803658e86e
phille10 <116751040+phille10@users.noreply.github.com>
2023-10-25 15:31:42 -0400

    adding package documentation

---
+++ R/emprac-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' emprac: A package with a minimum example of package creation.
+#'
+#' This package is an exercise in package creation using
+#' R studio. The package includes a sample function and
+#' a sample dataset with their respective documentation.
+#'
+#' @docType package
+#' @name emprac-package
+#' @author Elizabeth Phillips \email{phille10@@mcmaster.ca}
+#' @references \url{https://github.com/phille10/emprac}
+NULL
